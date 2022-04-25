dbfcd78c4a825501859df2fef016d6562dd0d044
nandana-b <90544797+nandana-b@users.noreply.github.com>
2022-04-25 11:48:30 +0400

    Add files via upload

---
+++ SetExercises-main/Chapter 6/index.js
@@ -0,0 +1,19 @@
+﻿/*function to convert fahrenheit to kelvin */
+function calculateFK() {
+    var fahrenh = document.fk.fahrenh.value;
+    var con = 32
+
+    var finalACC = ((fahrenh - 32) / 1.8) + 273.15;
+
+    document.fk.ad.value = (finalACC.toFixed(2) + "K" );
+
+}
+/*function to convert fahrenheit to celsius */
+function calculateFC() {
+    var fahrenh = document.fc.fahrenh.value;
+    var con = 32
+    var finalACC = (fahrenh - con) / 1.8;
+
+    document.fc.accc.value = (finalACC.toFixed(2) + "c");
+
+}
